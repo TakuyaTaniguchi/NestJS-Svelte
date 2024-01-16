fad67e5182b245fe303e424ff2cd0bbd34fedd5e
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-01-16 17:37:57 +0900

    Svelte基本

---
+++ frontend/src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let title = 'Home'
+</script>
+<div class="title">
+    <h1>{title}</h1>
+</div>
+
+<style>
+    .title {
+        padding: 0 16px;
+    }
+    h1 {
+        font-weight: bold;   
+        color: turquoise; 
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit 
-    <a href="https://kit.svelte.dev">kit.svelte.dev</a> 
-to read the documentation</p>
-<div>aaaaa</div>
+<script lang="ts">
+    import  Header  from '$lib/Header.svelte'
+</script>
+<div>
+    <Header title="+pageSvelte"/>
+</div>
+
+<style>
+</style>
