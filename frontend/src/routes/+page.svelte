493a1cf97bb53c878beef051168e907e4e7e9abf
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-01-16 21:33:47 +0900

    DBのデータを表示

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
     import  Header  from '$lib/Header.svelte'
     import { onMount } from 'svelte'
+    export type Item = {
+        id: number,
+        firstName: string,
+        lastName: string
+    }
+    let item : Item;
 
     onMount(async ()=>{
         const response = await fetch('http://localhost:3000/customers/sample');
-        const data = await response.json();
-        console.log(data);
-        console.log(data.id);
+        const data = await response.json() as Item;
+        item = data
+
     })
 </script>
+
+<style>
+.container {
+  display: flex;
+  align-items: flex-end;
+  flex-direction: row;
+  align-content: stretch;
+  justify-content: space-around;
+}
+
+</style>
+
 <div>
     <Header title="+pageSvelte"/>
     <div class="container">
         <div class="inner">
           <form action="customers/add" method="post" class="form-example">
             <div class="form-example">
-              <label for="firtstname">Enter your firtstname: </label>
-              <input type="text" name="firtstname" id="firtstname"  />
+              <label for="firstName">Enter your firstName: </label>
+              <input type="text" name="firstName" id="firstName"  />
             </div>
             <div class="form-example">
               <label for="lastname">Enter your lastname: </label>
@@ -26,6 +44,27 @@
               <input type="submit" value="add" />
             </div>
           </form>
+        </div>
+        <div class="inner">
+          <h2>登録ユーザー</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>firstName</th>
+                <th>lastname</th>
+              </tr>
+            </thead>
+            <tbody>
+              {#if item }
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                </tr>
+              {/if}
+            </tbody>
+          </table>
         </div>
       </div>
 </div>
