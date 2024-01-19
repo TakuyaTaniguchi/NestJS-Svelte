d55624431041a22bee35930ccf9dc0a65edc423d
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-01-19 19:30:08 +0900

    詳細IDを表示数r

--- frontend/src/routes/customer/[userId]/+page.svelte
+++ frontend/src/routes/customer/[userId]/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { page } from '$app/stores';
+  export let id : string
 
+  onMount(() => {
+    // $paramsからuserIdを取得
+    id = $page.params.userId
+    
+  });
 </script>
 
 
 <div>
     <div>
-      user 詳細ページ
+      user 詳細ページ {id}
     </div>
 </div>
