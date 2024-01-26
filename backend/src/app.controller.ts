cd2a8ad6f6574a0b01e581dc7b551a42a0eb75d1
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-01-26 19:53:40 +0900

    move testing

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,14 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  index() {
-    return {
-      message: 'Hello world!!!! Nest',
-      user: {
-        id: 100,
-        name: 'taro',
-      },
-    };
+  getHello(){
+    return 'Hello World!'
   }
 }
