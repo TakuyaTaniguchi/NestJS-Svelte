666a62e754b69d58a231132d2dc5e273421b23e2
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-01-26 20:42:54 +0900

    Fix import path in customer.entity.ts

--- backend/src/entity/customer.entity.ts
+++ backend/src/entity/customer.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { ICustomer } from './interfaces/customers.interface';
+import { ICustomer } from '../interfaces/customers.interface';
 @Entity()
 export class Customer implements ICustomer {
   @PrimaryGeneratedColumn()
