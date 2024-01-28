95184ed23f7b2b2de8c8bbc93440bc5870d48661
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-01-28 10:46:00 +0900

    Add @smui/drawer and @smui/list dependencies

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,8 +6,10 @@
 	import { apiCLient } from '../lib/core/index'
 
 	import Button, { Label } from '@smui/button';
-
-	let clicked = 0;
+	import Drawer, { AppContent, Content } from '@smui/drawer';
+ 	import List, { Item, Text } from '@smui/list';
+ 
+	let clicked = 0 as number | string;
 	let customers: Customer[];
 	let id: number;
 	let firstName: string;
@@ -48,11 +50,60 @@
 </script>
 
 <div>
-	<Header title="+pageSvelte" />
+	<!-- <Header title="+pageSvelte" />
 	<Button on:click={() => clicked++} class="myClass">
 		<Label>With a Class {clicked}</Label>
-	  </Button>
-	<div class="container">
+	  </Button> -->
+	  <div class="drawer-container">
+		<Drawer>
+		  <Content>
+			<List>
+			  <Item
+				href="javascript:void(0)"
+				on:click={() => (clicked = 'Gray Kittens')}
+			  >
+				<Text>Gray Kittens</Text>
+			  </Item>
+			  <Item
+				href="javascript:void(0)"
+				on:click={() => (clicked = 'A Space Rocket')}
+			  >
+				<Text>A Space Rocket</Text>
+			  </Item>
+			  <Item
+				href="javascript:void(0)"
+				on:click={() => (clicked = '100 Pounds of Gravel')}
+			  >
+				<Text>100 Pounds of Gravel</Text>
+			  </Item>
+			  <Item
+				href="javascript:void(0)"
+				on:click={() => (clicked = 'All of the Shrimp')}
+			  >
+				<Text>All of the Shrimp</Text>
+			  </Item>
+			  <Item
+				href="javascript:void(0)"
+				on:click={() => (clicked = 'A Planet with a Mall')}
+			  >
+				<Text>A Planet with a Mall</Text>
+			  </Item>
+			</List>
+		  </Content>
+		</Drawer>
+	   
+		<AppContent class="app-content">
+		  <main class="main-content">
+			App content.
+			<br />
+			{#if clicked === 'Gray Kittens'}
+				Gray Kittens
+			{/if}
+			<pre class="status">Clicked: {clicked}</pre>
+		  </main>
+		</AppContent>
+	  </div>
+	<!-- <div class="container">
 		<div class="inner">
 			<div class="form-example">
 				<div class="form-example">
@@ -105,7 +156,7 @@
 				on:removeCustomer={removeCustomer}
 			/>
 		</div>
-	</div>
+	</div> -->
 </div>
 
 <style>
@@ -115,4 +166,31 @@
 		align-content: stretch;
 		justify-content: space-around;
 	}
+
+  /* These classes are only needed because the
+    drawer is in a container on the page. */
+	.drawer-container {
+    position: relative;
+    display: flex;
+    height: 350px;
+    max-width: 100%;
+    border: 1px solid
+      var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+    overflow: hidden;
+    z-index: 0;
+  }
+ 
+  * :global(.app-content) {
+    flex: auto;
+    overflow: auto;
+    position: relative;
+    flex-grow: 1;
+  }
+ 
+  .main-content {
+    overflow: auto;
+    padding: 16px;
+    height: 100%;
+    box-sizing: border-box;
+  }
 </style>
