f40553f8e2d4d9bf8e4ce8f284548d0e3454489c
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-02-05 19:19:13 +0900

    Add @nestjs/swagger for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -8,8 +9,19 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('NestJS API')
+  .setDescription('The NestJS API description')
+  .setVersion('1.0')
+  .addTag('nestjs')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api',app,document)
+
   app.enableCors(corsOptionsDelegate);
-  await app.listen(3000);
+  await app.listen(4000);
   app.useStaticAssets(join(__dirname, '../src', 'public'));
   app.setBaseViewsDir(join(__dirname, '../src', 'views'));
   app.setViewEngine('hbs');
